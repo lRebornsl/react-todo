ac4395bfd2473b56bfc185c61c9b48e217487a06
lRebornsl <anthony_vasquez98@hotmail.com>
2023-07-12 12:54:43 -0500

    Add checkbox event

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,21 +1,17 @@
 import { FaTrash } from "react-icons/fa";
 import { AiFillEdit } from "react-icons/ai";
 
-const ListItem = ({todos}) => {
+const ListItem = ({itemProp, handleChange}) => {
   return(
-    todos.map((todo) => {
-      return(
-        <li className="flex justify-between bg-neutral-100 p-2 my-1" key={todo.id}>
-          <div className="flex">
-            <input className="mr-2" type="checkbox"></input>{todo.title}
-          </div>
-          <div>
-            <button><AiFillEdit /></button>
-            <button className="ml-1"><FaTrash /></button>
-          </div>
-        </li>
-      )
-    })
+    <li className="flex justify-between bg-neutral-100 p-2 my-1">
+      <div className="flex">
+        <input className="mr-2" type="checkbox" onChange={() => handleChange(itemProp.id)}></input>{itemProp.title}
+      </div>
+      <div>
+        <button><AiFillEdit /></button>
+        <button className="ml-1"><FaTrash /></button>
+      </div>
+    </li>
   );
 }
 
