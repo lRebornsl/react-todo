cf93efc3af9cb27b586774463d5677aa122b8136
lRebornsl <anthony_vasquez98@hotmail.com>
2023-07-12 09:19:08 -0500

    Add ListItem component

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,23 +1,10 @@
 import PropTypes from "prop-types";
-import { FaTrash } from "react-icons/fa";
-import { AiFillEdit } from "react-icons/ai";
+import ListItem from "./ListItem";
 
 const TodosList = ({todos}) => {
   return(
     <ul>
-      {todos.map((todo) => {
-        return(
-          <li className="flex justify-between bg-neutral-100 p-2 my-1" key={todo.id}>
-            <div className="flex">
-              <input className="mr-2" type="checkbox"></input>{todo.title}
-            </div>
-            <div>
-              <button><AiFillEdit /></button>
-              <button className="ml-1"><FaTrash /></button>
-            </div>
-          </li>
-        )
-      })}
+      <ListItem todos={todos} />
     </ul>
   );
 };
